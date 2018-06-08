382e0382ae68b7466a7b4885a13996bf484f38bc
zhaohejing <454258314@qq.com>
2018-06-08 13:21:15 +0800

    init

--- src/main/resources/dashboard/src/main.ts
+++ src/main/resources/dashboard/src/main.ts
@@ -18,40 +18,37 @@ if(!abp.utils.getCookieValue('Abp.Localization.CultureName')){
   let language=navigator.language;
   abp.utils.setCookieValue('Abp.Localization.CultureName',language,new Date(new Date().getTime() + 5 * 365 * 86400000),abp.appPath);
 }
-
-Ajax.get('/AbpUserConfiguration/GetAll').then(data=>{
-  Util.abp=Util.extend(true,Util.abp,data.data.result);
-  new Vue({
-    render: h => h(App),
-    router:router,
-    store:store,
-    data: {
-      currentPageName: ''
-    },
-    async mounted () {
-      this.currentPageName = this.$route.name as string;
-      await this.$store.dispatch({
-        type:'session/init'
-      })
-      if(!!this.$store.state.session.user&&this.$store.state.session.application.features['SignalR']){
-        if (this.$store.state.session.application.features['SignalR.AspNetCore']) {
-            SignalRAspNetCoreHelper.initSignalR();
-        }
+new Vue({
+  render: h => h(App),
+  router:router,
+  store:store,
+  data: {
+    currentPageName: ''
+  },
+  async mounted () {
+    this.currentPageName = this.$route.name as string;
+    await this.$store.dispatch({
+      type:'session/init'
+    })
+    if(!!this.$store.state.session.user&&this.$store.state.session.application.features['SignalR']){
+      if (this.$store.state.session.application.features['SignalR.AspNetCore']) {
+          SignalRAspNetCoreHelper.initSignalR();
       }
-      this.$store.commit('app/initCachepage');
-      this.$store.commit('app/updateMenulist');
-    },
-    created () {
-      let tagsList:Array<any> = [];
-      appRouters.map((item) => {
-          if (item.children.length <= 1) {
-              tagsList.push(item.children[0]);
-          } else {
-              tagsList.push(...item.children);
-          }
-      });
-      this.$store.commit('app/setTagsList', tagsList);
     }
-  }).$mount('#app')
-})
+    this.$store.commit('app/initCachepage');
+    this.$store.commit('app/updateMenulist');
+  },
+  created () {
+    let tagsList:Array<any> = [];
+    appRouters.map((item) => {
+        if (item.children.length <= 1) {
+            tagsList.push(item.children[0]);
+        } else {
+            tagsList.push(...item.children);
+        }
+    });
+    this.$store.commit('app/setTagsList', tagsList);
+  }
+}).$mount('#app')
+
 
